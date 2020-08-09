403d5f3681d48c1a81bf9b4f26d9013983fd7e4c
dtyunegov <tyunegov.dy@mail.ru>
2020-08-09 20:55:19 +0500

    Tests AddTransactionCommand

--- MoneyFllowTests/MainViewModelTests.cs
+++ MoneyFllowTests/MainViewModelTests.cs
@@ -1,18 +1,47 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MoneyFllow;
+using MoneyFllow.Model;
+using Moq;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MoneyFllow.Tests
 {
     [TestClass()]
     public class MainViewModelTests
-    {
-        [TestMethod()]
-        public void MainViewModelTest()
+    {        
+        [DataTestMethod]
+        [DataRow("", 1)]
+        [DataRow("test", 0)]
+        public void CanExecuteAddTransactionCommand_ReturnFalse(string category, int summ)
         {
-            Assert.Fail();
+            Transaction transaction = new Transaction()
+            {
+                Category = category,
+                Summ = summ,
+            };
+            MainViewModel main = new MainViewModel();
+            main.NewTransaction = transaction;
+            bool result = main.CanExecuteAddTransactionCommand();
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void CanExecuteAddTransactionCommand_ReturnTrue()
+        {
+            Transaction transaction = new Transaction(new DateTime(), "test", "", 123);
+            MainViewModel main = new MainViewModel();
+            main.NewTransaction = transaction;
+            bool result = main.CanExecuteAddTransactionCommand();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void ExecuteAddTransactionCommand()
+        {
+            Mock<ITransactionRepository> _transactionRepository = new Mock<ITransactionRepository>();
+            MainViewModel main = new MainViewModel(_transactionRepository.Object);
+            _transactionRepository.Setup(x=>x.Add(It.IsAny<Transaction>()));
+            main.ExecuteAddTransactionCommand();
+            _transactionRepository.VerifyAll();
         }
     }
 }
